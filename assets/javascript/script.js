aec1895f7b60003253f391abc09993c6514ea092
Paula Peroutka <paulaperoutka@gmail.com>
2018-11-26 23:59:24 -0500

    add onclick to change pages in nav

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,4 +3,9 @@ window.onload = function () {
   document.getElementById("edit-logo").onclick = function () {
     window.location.href = "./assets/pages/landing.html";
   };
+
+  document.getElementById("contact-nav-link").onclick = function () {
+    console.log("change to contact page");
+    window.location.href = "../pages/contact.html";
+  };
 };
